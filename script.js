bc15b330ab5285f6824cfdbff3a5243dd7cd70fe
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-10-26 18:59:39 -0500

    Added functions for random lowercase, uppercase, and special characters and then used the console to test this. I also added some notes above each section in the js file to keep myself organized.

--- script.js
+++ script.js
@@ -14,32 +14,60 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 
+// My work below this line
+
+
+
+
+
 
 // Variables with possible characters
-var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789" + "!@#$%&*,.;:";
-var specialCharacters = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
-var possibilities = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
- 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
- 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-
-
-
-
-
-
+// var characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" + "abcdefghijklmnopqrstuvwxyz0123456789" + "!@#$%&*,.;:";
+// var specialCharacters = "~`!#$%^&*+=-[]\\\';,/{}|\":<>?";
+// var possibilities = ['A', 'B', 'C', 'D', 'E', 'F', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+//  'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
+//  'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 
 
 // Prompts - Asking the user to select options before generating password based on their specifications
-var numberCharacters = prompt("How many characters would you like?");
-var specilCharacters = confirm("Do you want special characters?");
-var uppercaseCharacters = confirm("Do you want uppercase characters?");
-var lowercaseCharacters = confirm("Do you want lowercase characters?");
+// var numberCharacters = prompt("How many characters would you like?");
+// var specilCharacters = confirm("Do you want special characters?");
+// var uppercaseCharacters = confirm("Do you want uppercase characters?");
+// var lowercaseCharacters = confirm("Do you want lowercase characters?");
 
 
 // Functions in the works
 
-for (i = 1; i < characters.length; i++) {
-  alert("Your password is too short..please select between 8 & 125 characters.");
+// for (i = 1; i < characters.length; i++) {
+//   alert("Your password is too short..please select between 8 & 125 characters.");
+// }
+
+//Generator Functions
+
+// Generate a random number
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
 }
+
+// Generate random lowercase letter
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+// Generate random uppercase letter
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+// Generate random special character
+function getRandomSpecial() {
+  return String.fromCharCode(Math.floor(Math.random() * 15) + 33);
+}
+
+// Used function to check if my functions work properly and they do
+console.log(getRandomNumber());
+console.log(getRandomLower());
+console.log(getRandomUpper());
+console.log(getRandomSpecial());
 
