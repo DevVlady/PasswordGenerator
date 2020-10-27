a07e04f1ea17eac01bcb57f3186117eb8b23db2e
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-10-26 22:31:56 -0500

    Added the values to all my variables built so far numbers, lower, upper, special case.

--- script.js
+++ script.js
@@ -18,11 +18,17 @@ generateBtn.addEventListener("click", writePassword);
 
 //My variables
 var numberCharacters;
-var lowerCharacters;
 var specialCharacters;
-var uppercaseCharacters;
-var lowercaseCharacters;
+var upperCharacters;
+var lowerCharacters;
 
+
+//My arrays
+var begin = "";
+number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
+upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+special = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "?", "+", "`"]
 
 
 
