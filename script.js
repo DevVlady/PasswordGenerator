01a1cb5d141258ec5f13a5cd2bcdd4e0efd3ab50
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-10-27 21:57:13 -0500

    I changed the function around with my if and else statements in order for them to work properly as i was getting an erro when i shouldnt have.

--- script.js
+++ script.js
@@ -27,41 +27,38 @@ var password = [];
 
 //My arrays
 var begin = "";
-number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-special = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "?", "+", "`"];
+var number = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var upper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var special = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "?", "+", "`"];
 
 
 // Function that generates feedback based on what the user selects.
 
 function writePassword() {
+  //Will prompt a filed to enter a number of characters user would like
+  begin = parseInt(prompt("How many characters would you like? Please enter a number between 8 & 128"));
 
-  begin = parseInt(prompt("How many characters would you like? Please enter a number between 8 & 129"));
-  // If statement below will prompt alert mesaage if no value is entered.
-  if (!begin) {
-    alert("A value is needed in order to continue!");
-  }
 
-  else if (begin < 8 || begin > 129) {
-    //Checks users input to ensure it meets criteria
+  while  (begin < 8 || begin > 128) {
+    //Checks users input to ensure it meets criteria...will prompt message if criteria is not met
     alert("Please enter a vlaid length & try again!");
-    begin = parseInt(prompt("Please enter a number between 8 & 129"));
+    begin = parseInt(prompt("Please enter a number between 8 & 128"));
+  }
+
+  if (!begin) {
+    //WIll prompt the message below if user decides to escape the input field
+    alert("Please enter a value to conitnue!")
+  }
+  else {
+    //Will prompt the field criteria if user enters values with the parameters
     numberCharacters = confirm("Would you like numbers included in your password?");
     specialCharacters = confirm("Would you like special characters included in your password?");
     upperCharacters = confirm("Would you like uppercase letters included in your password?");
     lowerCharacters = confirm("Would you like lowercase letters included in your password?");
   }
-
-  else {
-    //If user enters numbers within the criteria then the alerts below will appear one after the other.
-    numberCharacters = confirm("Would you like numbers included in your password?");
-    specialCharacters = confirm("Would you like special characters included in your password?");
-    upperCharacters = confirm("Would you like uppercase letters included in your password?");
-    lowerCharacters = confirm("Would you like lowercase letters included in your password?");
-  };
-
-  if (!numberCharacters && !specialCharacters && !upperCharacters && !lowerCharacters) {
+  while (!numberCharacters && !specialCharacters && !upperCharacters && !lowerCharacters) {
+    //If no options are seelcted then it will prompt the message below
     options = alert("Error! Please select at least one of the of following options.");
     numberCharacters = confirm("Would you like numbers included in your password?");
     specialCharacters = confirm("Would you like special characters included in your password?");
@@ -69,8 +66,14 @@ function writePassword() {
     lowerCharacters = confirm("Would you like lowercase letters included in your password?");
   }
 
+
+  //Code Works up until the lines below**************
+
+
   //Creating the for variable whcih will help when generating random password
-  // for (var i = 0)
+  // for (var i = 0; i < begin; i++) {
+
+  // }
 
 
 
