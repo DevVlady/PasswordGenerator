c226c16e97945670129d7d825d08be61fd343075
Vladimir <vladimir.s.dimitrov14@gmail.com>
2020-10-29 01:18:18 -0500

    Added my arrays & empty arrays which i had already created so i copied them from my listed code below.

--- script.js
+++ script.js
@@ -25,6 +25,14 @@ var confirmLowercase;
 var confirmSpecial;
 
 
+//My arrays & empty arrys that will store my responses
+var choices = "";
+
+var numberCharacters = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var lowercaseCharacters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var uppercaseCharacters = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
+var specialCharacters = ["~", "!", "@", "#", "$", "%", "^", "&", "*", "(", ")", ",", ".", "?", "+", "`"];
+
 
 
 
